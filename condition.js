68842ae33f98a85f69df0eea4d3a78bc9f7bbc01
adnanalemran <adnanalemranontor@gmail.com>
2023-02-06 03:19:47 +0600

    con

--- condition.js
+++ condition.js
@@ -1,1 +1,11 @@
-console.log("helllo world");
+var name = "hablu";
+
+if (name == "maklu") {
+     console.log("this name is Maklu");
+}
+if(name=="hablu"){
+     console.log("this hablu \n");
+}
+else{
+     console.log("no not fund");
+}
