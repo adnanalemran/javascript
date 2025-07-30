1e3396ce5eab7b3615e814326a11821b359a02e2
adnanalemran <adnanalemranontor@gmail.com>
2025-07-30 13:54:23 +0600

    Add initial implementation of names traversal with callback function

---
+++ callback.js
@@ -0,0 +1,13 @@
+var names  = ['adnan', 'rakib ', 'rafid', 'robi ','rasel'];
+
+function traverse(names,callback){
+
+    for(var i= 0 ; i<names.length;i++){
+        callback(names[i])
+    }
+
+}
+
+traverse(names, function(name){
+    console.log(name.toUpperCase());
+})
